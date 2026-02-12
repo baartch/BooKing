2ab5e253a7db6de743e586d22ab66568c88c8a07
baartch <andy@baart.ch>
2026-02-12 18:36:58 +0100

    💄 Clean up communication email styles

--- app/pages/communication/email.php
+++ app/pages/communication/email.php
@@ -2,7 +2,7 @@
 require_once __DIR__ . '/../../src-php/communication/email_view.php';
 ?>
 <div class="box">
-  <div class="columns is-variable is-3 email-columns has-text-primary-40">
+  <div class="columns is-variable is-3 email-columns">
     <?php require __DIR__ . '/email_sidebar.php'; ?>
     <?php require __DIR__ . '/email_list.php'; ?>
     <?php require __DIR__ . '/email_detail.php'; ?>
